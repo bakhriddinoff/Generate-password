ec8b61ee99d1df6d2f004ff14441d7b0648b58a9
Ozodbek <bakhriddinoff01@gmail.com>
2025-10-17 21:12:19 +0500

    added content

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,22 @@ function App() {
 
 
   return (
-    <div>
+    <div className={`h-[100vh] flex flex-col justify-start items-start gap-5 pt-30 pl-150 relative ${theme === "dark" ? "bg-[#1F2937]" : "bg-[#ECFDF5]"}`}>
+      <button 
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")} 
+      className={`w-[100px] h-[35px] absolute left-5 top-5 rounded ${theme === "dark" ? "bg-white text-black" : "text-white bg-black" }`}>{theme === "dark mode" ? "Light mode" : "Dark mode"}</button>
+
+    <h1 className={`text-[40px] font-bold leading-10 ${theme === "dark" ? "text-white" : "text-[#111827]"}`}>Generate a <br />  <span className="text-green-400">random password</span></h1>
+    <p className={` text-[20px] ${theme === "dark" ? "text-[#D5D4D8]" : "text-black"}`}>Never use an insecure password again.</p>
+    <button onClick={handleGenerate} className="bg-[#10B981] h-[42px] w-[191px] rounded-[6px] text-white mt-10 mb-5 cursor-pointer hover:bg-[#11e29d]">Generate passwords</button>
+    <hr  className="border-[#273549] w-[500px]"/>
+    {/* btn-wrapper */}
+    <div className="flex gap-5">
+      <button className="bg-[#273549] h-[39px] w-[211px] rounded-[6px] text-[#10B981] mt-5">{password1}</button>
+      <button className="bg-[#273549] h-[39px] w-[211px] rounded-[6px] text-[#10B981] mt-5">{password2}</button>
     </div>
+    </div>
+
   )
 }
 
