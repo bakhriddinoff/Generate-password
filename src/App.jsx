35f90ef9d537aa9de97143ae2771ff2968c93b19
Ozodbek <bakhriddinoff01@gmail.com>
2025-10-17 21:10:14 +0500

    added variables and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react"
+
+
 function App() {
+
+  const [password1, setPassword1] = useState("")
+  const [password2, setPassword2] = useState("") 
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
+  const generatePassword = (length = 7) =>  {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let pass = "";
+    for (let i = 0; i < length; i++) {
+      pass += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+     return pass
+  }
+
+  const handleGenerate = () => {
+    setPassword1(generatePassword());
+    setPassword2(generatePassword());
+  }
+
+
 
   return (
     <div>
@@ -7,4 +34,3 @@ function App() {
 }
 
 export default App
-
